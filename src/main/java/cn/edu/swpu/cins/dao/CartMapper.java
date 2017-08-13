dce4f12c57eb9c878e52db4714ed60c7abb9b792
bohenmian <398206896@qq.com>
2017-08-13 17:45:05 +0800

    Initial Project

---
+++ src/main/java/cn/edu/swpu/cins/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package cn.edu.swpu.cins.dao;
+
+import cn.edu.swpu.cins.entity.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
