b0caa11e5d82545a8211bfa1b1c1d27425fa64eb
bohenmian <398206896@qq.com>
2017-08-21 17:24:05 +0800

    add cart view

---
+++ src/main/java/cn/edu/swpu/cins/dto/view/CartVo.java
@@ -0,0 +1,54 @@
+package cn.edu.swpu.cins.dto.view;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CartVo {
+
+    private List<CartProductVo> cartProductVoList;
+    private BigDecimal cartTotalPrice;
+    private boolean allChecked;
+    private String imageHost;
+
+    public CartVo() {
+    }
+
+    public CartVo(List<CartProductVo> cartProductVoList, BigDecimal cartTotalPrice, boolean allChecked, String imageHost) {
+        this.cartProductVoList = cartProductVoList;
+        this.cartTotalPrice = cartTotalPrice;
+        this.allChecked = allChecked;
+        this.imageHost = imageHost;
+    }
+
+    public List<CartProductVo> getCartProductVoList() {
+        return cartProductVoList;
+    }
+
+    public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
+        this.cartProductVoList = cartProductVoList;
+    }
+
+    public BigDecimal getCartTotalPrice() {
+        return cartTotalPrice;
+    }
+
+    public void setCartTotalPrice(BigDecimal cartTotalPrice) {
+        this.cartTotalPrice = cartTotalPrice;
+    }
+
+    public boolean isAllChecked() {
+        return allChecked;
+    }
+
+    public void setAllChecked(boolean allChecked) {
+        this.allChecked = allChecked;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
