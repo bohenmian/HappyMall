6d05568460e9d56589bfceddbf0dff2b4386204c
bohenmian <398206896@qq.com>
2017-08-25 17:45:13 +0800

    add pay type enum

---
+++ src/main/java/cn/edu/swpu/cins/enums/PaymentTypeEnum.java
@@ -0,0 +1,29 @@
+package cn.edu.swpu.cins.enums;
+
+public enum PaymentTypeEnum {
+
+    ONLINE_PAY(1,"支付宝");
+    private int code;
+    private String value;
+
+    PaymentTypeEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
